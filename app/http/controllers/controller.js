97093f5fbbd71c3d72b77ddfc999d0329f0afbbd
Morteza1398 <morteza.es218@gmail.com>
2018-08-23 11:18:03 +0430

    24

--- app/http/controllers/controller.js
+++ app/http/controllers/controller.js
@@ -1,6 +1,7 @@
 const autoBind = require('auto-bind');
 const Recaptcha = require('express-recaptcha').Recaptcha;
 const { validationResult } = require('express-validator/check');
+const isMongoId = require('validator/lib/isMongoId');
 
 module.exports = class controller {
     constructor() {
@@ -46,5 +47,16 @@ module.exports = class controller {
     back(req , res) {
         req.flash('formData' , req.body);
         return res.redirect(req.header('Referer') || '/');
+    }
+
+    isMongoId(paramId) {
+        if(! isMongoId(paramId))
+            this.error('ای دی وارد شده صحیح نیست', 404);
+    }
+
+    error(message , status = 500) {
+        let err = new Error(message);
+        err.status = status;
+        throw err;
     }
 }
--- app/routes/web/index.js
+++ app/routes/web/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // Middlewares
 const redirectIfAuthenticated = require('app/http/middleware/redirectIfAuthenticated');
 const redirectIfNotAdmin = require('app/http/middleware/redirectIfNotAdmin');
+const errorHandler = require('app/http/middleware/errorHandler');
 
 // Admin Router
 const adminRouter = require('app/routes/web/admin');
@@ -16,6 +17,11 @@ router.use('/' , homeRouter);
 // Auth Router
 const authRouter = require('app/routes/web/auth');
 router.use('/auth' , redirectIfAuthenticated.handle ,authRouter);
+
+// Handle Errors
+router.all('*' , errorHandler.error404);
+router.use(errorHandler.handler)
+
 
 
 module.exports = router;
