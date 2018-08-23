f5f7003c9a0492779574352984a0626f4917eb4b
Morteza1398 <morteza.es218@gmail.com>
2018-08-23 11:44:52 +0430

    56

--- config/index.js
+++ config/index.js
@@ -11,5 +11,8 @@ module.exports = {
     port : process.env.APPLICATION_PORT,
     cookie_secretkey : process.env.COOKIE_SECRETKEY,
     debug : true,
-    siteurl : process.env.WEBSITE_URL
+    siteurl : process.env.WEBSITE_URL,
+    jwt : {
+        secret_key : 'fgsdget#r%!@#$qeg'
+    }
 }
